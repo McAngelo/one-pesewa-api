13b679f7cd7a38acf41ad55f3937d178d65968a9
Michael Johnson <mcangelo200@gmail.com>
2021-01-10 09:00:16 +0000

    declared other files

--- app.js
+++ app.js
@@ -8,23 +8,20 @@ const express = require('express'),
       url = require('url'),
       app = express();
 
-app.get('/contacts', 
-		function(request, response){
-			var get_params = url.parse(request.url, true).query;	
-			
-			if (Object.keys(get_params).length === 0)
-			{
-				response.setHeader('content-type', 'application/json');
-				response.end(JSON.stringify(contacts.list()));	
-			}
-			else
-			{
-				response.setHeader('content-type', 'application/json');
-				response.end(JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value)));
-			}
-		}
-);
+app.get('/contacts', function(request, response){
+    var get_params = url.parse(request.url, true).query;	
 
+    if (Object.keys(get_params).length === 0)
+    {
+        response.setHeader('content-type', 'application/json');
+        response.end(JSON.stringify(contacts.list()));	
+    }
+    else
+    {
+        response.setHeader('content-type', 'application/json');
+        response.end(JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value)));
+    }
+});
 
 app.get('/contacts/:number', function(request, response) {
 	response.setHeader('content-type', 'application/json');	
