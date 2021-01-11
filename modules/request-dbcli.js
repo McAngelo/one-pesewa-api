813c47debcbc7948c41f94796b618c506e0914f1
Michael Johnson <mcangelo200@gmail.com>
2021-01-11 10:37:56 +0000

    --worked on therequest db cli

---
+++ modules/request-dbcli.js
@@ -0,0 +1,18 @@
+const request = require('request'),
+      url = 'http://127.0.0.1:5984',
+      db = 'books',
+      method = process.argv[2] || 'GET',
+      options = {
+          method: method,
+          url: `${url}/${db}/${process.argv[4] || ''}`
+      };
+console.log(process.argv[3], process.argv[4]);
+console.log(options.url);
+
+request(options, function(err, res, body){
+    if(err){
+        throw Error(err);
+    }else{
+        console.log(res.statusCode, JSON.parse(body));
+    }
+});
